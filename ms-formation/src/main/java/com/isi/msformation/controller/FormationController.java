558098f1ef685da81688b54d35e279a33a28759c
abderrahmaneknc <a.kennouche@esi-sba.dz>
2026-04-21 10:15:51 +0100

    added api

--- ms-formation/src/main/java/com/isi/msformation/controller/FormationController.java
+++ ms-formation/src/main/java/com/isi/msformation/controller/FormationController.java
@@ -3,6 +3,8 @@ package com.isi.msformation.controller;
 import com.isi.msformation.Entities.Formation;
 import com.isi.msformation.Repositories.FormationRep;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
+@RefreshScope
 public class FormationController {
 
     @Autowired
@@ -19,4 +22,14 @@ public class FormationController {
     public Formation getFormation(@PathVariable Long id) {
         return formationRep.findById(id).get();
     }
+
+    @Value("${mes-config.email}")
+    private String email;
+
+    @GetMapping("email")
+    public String getEmail(){
+        return email;
+    }
+
+
 }
